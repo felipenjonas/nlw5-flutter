6c85e7cda1c36f2e8e48065d61ddf3a87e000346
Felipe Jonas <felipenjonas@gmail.com>
2021-04-20 23:29:44 -0300

    change to add new widget

--- devquiz/lib/core/app_widget.dart
+++ devquiz/lib/core/app_widget.dart
@@ -1,12 +1,14 @@
-import 'package:DevQuiz/home/home_page.dart';
-import 'package:DevQuiz/splash/splash_page.dart';
+// import 'package:DevQuiz/home/home_page.dart';
+import 'package:devquiz/home/home_page.dart';
+// import 'package:devquiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "DevQuiz",
+      title: "devquiz",
+      // home: SplashPage(), Vai mostrar a splash pra poder ser configurada
       home: HomePage(),
     );
   }
