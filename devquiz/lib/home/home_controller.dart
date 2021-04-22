9d0087bbf6eab3bb55031fa935d6b599c5e6d404
Felipe Jonas <felipenjonas@gmail.com>
2021-04-21 22:51:14 -0300

    add db conversion and consumption

--- devquiz/lib/home/home_controller.dart
+++ devquiz/lib/home/home_controller.dart
@@ -1,4 +1,5 @@
 import 'package:devquiz/core/app_images.dart';
+import 'package:devquiz/home/home_repository.dart';
 import 'package:devquiz/home/home_state.dart';
 import 'package:devquiz/models/awnser_model.dart';
 import 'package:devquiz/models/question_model.dart';
@@ -14,48 +15,17 @@ class HomeController {
   UserModel? user;
   List<QuizModel>? quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-
-    user = UserModel(
-      name: "Felipe DEV",
-      photoUrl: "https://avatars.githubusercontent.com/u/48365582?v=4",
-    );
+    user = await repository.getUser();
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-    await Future.delayed(Duration(seconds: 2));
-    quizzes = [
-      QuizModel(
-        title: "NLW 5 Flutter",
-        imagem: AppImages.blocks,
-        questionAnswered: 1,
-        level: Level.facil,
-        questions: [
-          QuestionModel(
-            title: "Está curtindo o Flutter?",
-            awnsers: [
-              AwnserModel(title: "Estou curtindo!"),
-              AwnserModel(title: "Estou Amando o Flutter!", isRight: true),
-              AwnserModel(title: "Ainda não sei o que dizer."),
-              AwnserModel(title: "Não quero nunca mais ver ele."),
-            ],
-          ),
-          QuestionModel(
-            title: "Está curtindo o Flutter?",
-            awnsers: [
-              AwnserModel(title: "Estou curtindo!"),
-              AwnserModel(title: "Estou Amando o Flutter!", isRight: true),
-              AwnserModel(title: "Ainda não sei o que dizer."),
-              AwnserModel(title: "Não quero nunca mais ver ele."),
-            ],
-          )
-        ],
-      )
-    ];
+    quizzes = await repository.getQuizzes();
     state = HomeState.success;
   }
 }
