9c3ad927abe16dcd81f157c6d9f95ecba2a7d9de
Felipe Jonas <felipenjonas@gmail.com>
2021-04-23 22:11:05 -0300

    fix quiz widget

--- devquiz/lib/challenge/widgets/quiz/quiz_widget.dart
+++ devquiz/lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,11 +1,27 @@
 import 'package:devquiz/challenge/widgets/awnser/awnser_widget.dart';
 import 'package:devquiz/core/app_text_styles.dart';
+import 'package:devquiz/models/awnser_model.dart';
 import 'package:devquiz/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
-  const QuizWidget({Key? key, required this.question}) : super(key: key);
+  final VoidCallback onChange;
+
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AwnserModel awnser(int index) => widget.question.awnsers[index];
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +30,25 @@ class QuizWidget extends StatelessWidget {
         children: [
           SizedBox(height: 64),
           Text(
-            question.title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          ...question.awnsers
-              .map(
-                (e) => AwnserWidget(isRight: e.isRight, title: e.title),
-              )
-              .toList(),
+          for (var i = 0; i < widget.question.awnsers.length; i++)
+            AwnserWidget(
+              awnser: awnser(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange());
+              },
+            ),
         ],
       ),
     );
