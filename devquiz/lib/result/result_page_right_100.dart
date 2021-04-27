b274e4b84c975a5f31402369378b7cce348a2447
Felipe Jonas <felipenjonas@gmail.com>
2021-04-26 21:29:08 -0300

    change page

---
+++ devquiz/lib/result/result_page_right_100.dart
@@ -0,0 +1,108 @@
+import 'package:devquiz/challenge/widgets/next_button/next_button_widget.dart';
+import 'package:devquiz/core/app_images.dart';
+import 'package:devquiz/core/app_text_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
+
+class ResultPage extends StatelessWidget {
+  final String title;
+  final int length;
+  final int result;
+
+  const ResultPage({
+    Key? key,
+    required this.title,
+    required this.length,
+    required this.result,
+  }) : super(key: key);
+
+  shareMyUtilization() {
+    double utilization = result / length;
+    double myUtilizationPercent = utilization * 100;
+    String utilizationConverted = myUtilizationPercent.toStringAsFixed(0);
+    Share.share(
+        "QuizDev-\nEu obtive $utilizationConverted % de aproveitamento no Quiz $title!");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        padding: EdgeInsets.only(top: 100),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(AppImages.trophy),
+            Column(
+              children: [
+                Text(
+                  "Parabéns!",
+                  style: AppTextStyles.heading40,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text.rich(
+                  TextSpan(
+                    text: "Você concluiu",
+                    style: AppTextStyles.body,
+                    children: [
+                      TextSpan(
+                        text: "\n$title",
+                        style: AppTextStyles.bodyBold,
+                      ),
+                      TextSpan(
+                        text: "\ncom $result de $length acertos",
+                        style: AppTextStyles.body,
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 68),
+                        child: NextButtonWidget.purple(
+                          label: "Compartilhar",
+                          onTap: () {
+                            shareMyUtilization();
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 68),
+                        child: NextButtonWidget.white(
+                          label: "Voltar ao Início",
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
